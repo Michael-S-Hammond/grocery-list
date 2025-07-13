d2795248c47f5e55b116c1393dde06cf9dbeb80d
Michael Hammond <michael@torql.com>
2025-07-13 07:57:46 -0500

    Add placeholder frontend project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
-    import express from 'express';
+import express from 'express';
 
-    const app = express();
-    const port = 3000;
+const app = express();
+const port = 3000;
 
-    app.get('/', (req, res) => {
-      res.send('Hello from TypeScript Node.js Backend!');
-    });
+// Allow all cross origin for development and testing. This should be refined later.
+// Resource: https://auth0.com/blog/cors-tutorial-a-guide-to-cross-origin-resource-sharing/
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+});
 
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
-    
+app.get('/', (req, res) => {
+	res.send('Hello from TypeScript Node.js Backend!');
+});
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
